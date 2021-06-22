6a8141417774556825663c5075c4f5b97190388b
“hhy” <1614561214@qq.com>
2021-06-22 12:15:08 +0800

    first commit

---
+++ src/test/java/lemon/TestLogin.java
@@ -0,0 +1,16 @@
+package lemon;
+
+import org.testng.annotations.Test;
+
+/**
+ * @Project: test
+ * @Author: fanfan
+ * @Create: 2021-06-22 11:59
+ * @Desc：
+ **/
+public class TestLogin {
+    @Test
+    public void testLogin(){
+        System.out.println("hello world");
+    }
+}
